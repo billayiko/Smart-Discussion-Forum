81aa4759db4384fb416e2fe0bb66bf420387269a
smglao8 <smglao8@gmail.com>
2026-07-23 13:08:26 +0300

    student dashboaed 	modified:   app/Models/Quiz.php

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Carbon;
+use Carbon\CarbonInterface;
 
 class Quiz extends Model
 {
@@ -73,7 +73,7 @@ class Quiz extends Model
         $this->update(['questions_finalized_at' => now()]);
     }
 
-    public function endsAt(): ?Carbon
+    public function endsAt(): ?CarbonInterface
     {
         return $this->scheduled_at?->copy()->addMinutes($this->duration_minutes);
     }
